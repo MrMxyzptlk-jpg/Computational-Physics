eebadba9f85bae71ba75e77f77aa20f02bf05011
Jeronimo_AP <52756722+MrMxyzptlk-jpg@users.noreply.github.com>
2025-04-03 14:16:53 -0300

    Add files via upload

---
+++ guia1/ex2/run.sh
@@ -0,0 +1,14 @@
+gfortran ./modulos/precision.f90\
+        ./modulos/funciones.f90\
+        ./modulos/subrutinas.f90\
+        ex2.f90 -o ex2.exe 
+
+gfortran -c  ./modulos/funciones.f90\
+        ./modulos/subrutinas.f90\
+
+gfortran ex2.f90 funciones.o subrutinas.o -o ex2.exe -lfftw3 -lfftw3_threads -ffpe-trap=invalid,overflow,zero
+
+
+./ex2.exe             # Executable file
+
+#gnuplot plot.gp     # Plotting file
