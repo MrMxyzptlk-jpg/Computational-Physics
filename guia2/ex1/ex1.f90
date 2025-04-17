102294bb04958339f41b910565fa136313b793b5
Jeronimo_AP <52756722+MrMxyzptlk-jpg@users.noreply.github.com>
2025-04-16 22:14:55 -0300

    Update ex1.f90

--- guia2/ex1/ex1.f90
+++ guia2/ex1/ex1.f90
@@ -3,12 +3,12 @@
 ! Program: ex1.f90
 ! Purpose: numerical solution of the heat equation with null Dirichlet boundary condition
 !
-! Description: the input data must be specified in the metric system. The program solves the non-dimensional heat equation and transforms to the proper units using a conversion factor at the end. The jobs are selected with the logical type variables "do_forward_Euler", "do_backward_Euler", "do_Crank_Nicolson", "do_analitical_sol" and "do_analitical_conv", and all data is written to a directory "./datos" under file names "forward_euler.out", "backward_euler.out" and "crank_nicolson.out" respectively.
+! Description: the input data must be specified in the metric system. The program solves the non-dimensional heat equation and transforms to the proper units using a conversion factor at the end. The jobs are selected with the logical type variables "do_forward_Euler", "do_backward_Euler", "do_Crank_Nicolson", "do_analitical_sol" and "do_analitical_conv"
 !
 ! Input: The input data must be specified in an input file named "input.xml" and an example is provided in this repository. All arguments have a default value, and all jobs are set to FALSE as default. 
 !
 !
-! Output:
+! Output: all data is written to a directory "./datos" under file names indicating the task done. 
 !
 !
 !
